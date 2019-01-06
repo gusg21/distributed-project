b4c253e48047c0444a7112bec5201407c94235d4
gusg21 <gusg21@parkschool.net>
2019-01-06 16:26:35 -0500

    tenative midpoint position difference handling in the FNK

--- DistributedGame/Networking/FNK.cs
+++ DistributedGame/Networking/FNK.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,14 @@ namespace DistributedGame.Networking
 {
     class FNK
     {
+        // The position the FNK has for the player
+        public Vector2 remotePosition;
 
+        public Vector2 GetRenderPosition(Vector2 local)
+        {
+            // Return the midpoint between the position the FNK has
+            // and the position the player should be at
+            return (remotePosition - local) / 2;
+        }
     }
 }
