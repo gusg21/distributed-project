32f5d14c7a848f438f4ae442a848714284584482
gusg21 <gusg21@parkschool.net>
2019-01-06 16:43:11 -0500

    removed bad code

--- DistributedGame/Networking/FNK.cs
+++ DistributedGame/Networking/FNK.cs
@@ -9,14 +9,10 @@ namespace DistributedGame.Networking
 {
     class FNK
     {
-        // The position the FNK has for the player
-        public Vector2 remotePosition;
-
-        public Vector2 GetRenderPosition(Vector2 local)
-        {
-            // Return the midpoint between the position the FNK has
-            // and the position the player should be at
-            return (remotePosition - local) / 2;
-        }
+        /*
+         * - Get the room data in a given direction and move in that direction
+         * - Get the data for a given castle
+         * - Other high level FNK interfacing
+         */
     }
 }
---
+++ DistributedGame/Networking/P2P.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistributedGame.Networking
+{
+    class P2P
+    {
+        /*
+         * - Manages all of the P2P relations
+         * - Ideally has nice functions to call for updating your position
+         */
+    }
+}
