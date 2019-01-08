e96127cc567b46f214d225f4c1a21f0636293f83
gusg21 <gusg21@parkschool.net>
2019-01-08 13:54:53 -0500

    fnk basic

---
+++ FunNetworkKing/FNK.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunNetworkKing
+{
+    class FNK
+    {
+        Socket listeningSocket;
+
+        public void Run()
+        {
+            listeningSocket.Bind(new IPEndPoint(new IPAddress(b"localhost"), ));
+        }
+    }
+}
---
+++ FunNetworkKing/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunNetworkKing
+{
+    class Program
+    {
+        Socket socket;
+
+        static void Main(string[] args)
+        {
+            socket = new Socket(SocketType.Stream, ProtocolType.Tcp);
+            
+        }
+    }
+}
